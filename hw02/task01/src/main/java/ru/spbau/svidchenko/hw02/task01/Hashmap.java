7a1bbb6cfbcb906ce63019412ca090172276f18d
ArgentumWalker <svidchen-ko@amil.ru>
2016-09-18 00:37:21 +0300

    Comments

--- hw02/task01/src/main/java/ru/spbau/svidchenko/hw02/task01/Hashmap.java
+++ hw02/task01/src/main/java/ru/spbau/svidchenko/hw02/task01/Hashmap.java
@@ -5,7 +5,6 @@ package ru.spbau.svidchenko.hw02.task01;
  *      File contains Hashmap class only.
  *      (c) by Oleg Svidchenko
  */
-
 public class Hashmap {
     private final int INIT_SIZE = 127;
 
@@ -14,40 +13,39 @@ public class Hashmap {
     private int mod;
 
     /**
-     *Constructor for a Hashmap
-     *Calls Hashmap::clear() inside
+     * Constructor for a Hashmap
      */
     public Hashmap() {
         clear();
     }
 
     /**
-     *@return count of elements in Hashmap
+     * @return count of elements in Hashmap
      */
     public int size() {
         return count;
     }
 
     /**
-     *@param key is the key of pair you'r looking for
-     *@return true if contains any element with such key
+     * @param key is the key of pair you'r looking for
+     * @return true if contains any element with such key
      */
     public boolean contains(String key) {
         return (elements[key.hashCode() % mod].find(key) != null);
     }
 
     /**
-     *@param key is the key of element you'r looking for
-     *@return null if don't contains any element with such key or value of such element otherwise
+     * @param key is the key of element you'r looking for
+     * @return null if don't contains any element with such key or value of such element otherwise
      */
     public String get(String key) {
         return elements[key.hashCode() % mod].find(key);
     }
 
     /**
-     *@param key is the key of element you would like to insert
-     *@param value is the value of element you would like to insert
-     *@return previous value of element with such key or null if don't contains such element
+     * @param key is the key of element you would like to insert
+     * @param value is the value of element you would like to insert
+     * @return previous value of element with such key or null if don't contains such element
      */
     public String put(String key, String value) {
         String result = elements[key.hashCode() % mod]
@@ -62,8 +60,8 @@ public class Hashmap {
     }
 
     /**
-     *@param key is the key of element you'r want to remove
-     *@return null if don't contains any element with such key or value of such element otherwise
+     * @param key is the key of element you'r want to remove
+     * @return null if don't contains any element with such key or value of such element otherwise
      */
     public String remove(String key) {
         String result = elements[key.hashCode() % mod].findAndRemove(key);
@@ -74,7 +72,7 @@ public class Hashmap {
     }
 
     /**
-     *remove all elements from hashmap and resize it
+     * remove all elements from hashmap and resize it
      */
     public void clear() {
         int i;
