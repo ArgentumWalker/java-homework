3fc298da0b500f096438a8e4cfbc5c59dd7c57d3
ArgentumWalker <svidchen-ko@amil.ru>
2016-09-21 21:06:22 +0300

    Some minor fixes

--- hw02/task01/src/main/java/ru/spbau/svidchenko/hw02/task01/Hashmap.java
+++ hw02/task01/src/main/java/ru/spbau/svidchenko/hw02/task01/Hashmap.java
@@ -1,52 +1,39 @@
-package ru.spbau.svidchenko.hw02.task01;
+package ru.spbau.svidchenko.hashmap;
+
+import Java.lang.Math;
 
 /**
  * Resizable hashmap with lists inside without any optimizations.
- *      File contains Hashmap class only.
- *      (c) by Oleg Svidchenko
+ *      File contains Hashmap class.
  */
 public class Hashmap {
     private final int INIT_SIZE = 127;
-
+    
     private int count;
     private List[] elements;
     private int mod;
-
-    /**
-     * Constructor for a Hashmap
-     */
+    
+    /**Constructor for a Hashmap*/
     public Hashmap() {
         clear();
     }
-
-    /**
-     * @return count of elements in Hashmap
-     */
+    
+    /**Return count of elements in Hashmap*/
     public int size() {
         return count;
     }
-
-    /**
-     * @param key is the key of pair you'r looking for
-     * @return true if contains any element with such key
-     */
+    
+    /**Return true if contains any element with such key*/
     public boolean contains(String key) {
         return (elements[key.hashCode() % mod].find(key) != null);
     }
-
-    /**
-     * @param key is the key of element you'r looking for
-     * @return null if don't contains any element with such key or value of such element otherwise
-     */
+    
+    /**Return null if don't contains any element with such key or value of such element otherwise */ 
     public String get(String key) {
         return elements[key.hashCode() % mod].find(key);
     }
-
-    /**
-     * @param key is the key of element you would like to insert
-     * @param value is the value of element you would like to insert
-     * @return previous value of element with such key or null if don't contains such element
-     */
+    
+    /**Return previous value of element with such key or null if don't contains such element*/
     public String put(String key, String value) {
         String result = elements[key.hashCode() % mod]
                 .findAndReplace(key, value);
@@ -58,44 +45,39 @@ public class Hashmap {
         }
         return result;
     }
-
-    /**
-     * @param key is the key of element you'r want to remove
-     * @return null if don't contains any element with such key or value of such element otherwise
-     */
+    
+    /**Return null if don't contains any element with such key or value of such element otherwise*/         
     public String remove(String key) {
-        String result = elements[key.hashCode() % mod].findAndRemove(key);
-        if (result != null) {
+        String result = elements[Math.abs(key.hashCode()) % mod].findAndRemove(key);
+        if (result == null) {
             count--;
         }
         return result;
-    }
-
-    /**
-     * remove all elements from hashmap and resize it
-     */
+    }  
+    
+    /**remove all elements from hashmap and resize it*/
     public void clear() {
         int i;
-        count = 0;
+        count = 0; 
         elements = new List[INIT_SIZE];
         mod = INIT_SIZE;
         for (i = 0; i < elements.length; i++) {
             elements[i] = new List ();
         }
-    }
-
+    } 
+    
     private void sizeup() {
-        int i;
         mod *= 2;
         final List[] newElements = new List[mod];
-        for (i = 0; i < elements.length; i++) {
+        for (int i = 0; i < elements.length; i++) {
             newElements[elements[i].hashCode() % mod] = elements[i];
         }
-        for (i = 0; i < newElements.length; i++) {
+        for (int i = 0; i < newElements.length; i++) {
             if (newElements[i] == null) {
-                newElements[i] = new List ();
+                newElements[i] = new List();
             }
         }
     }
 }
+
 
