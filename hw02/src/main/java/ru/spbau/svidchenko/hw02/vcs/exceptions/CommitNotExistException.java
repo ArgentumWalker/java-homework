005ede3af39f8c16d016d8b4d606c3b7c5bfb087
ArgentumWalker <svidchen-ko@mail.ru>
2017-03-23 14:34:30 +0300

    another minor fixes

---
+++ hw02/src/main/java/ru/spbau/svidchenko/hw02/vcs/exceptions/CommitNotExistException.java
@@ -0,0 +1,7 @@
+package ru.spbau.svidchenko.hw02.vcs.exceptions;
+
+/**
+ * Represents that branch didn't exist
+ */
+public class CommitNotExistException extends VCSException {
+}
---
+++ hw02/src/main/java/ru/spbau/svidchenko/hw02/vcs/exceptions/VCSException.java
@@ -0,0 +1,7 @@
+package ru.spbau.svidchenko.hw02.vcs.exceptions;
+
+/**
+ * Any kind of VCS exceptions
+ */
+public class VCSException extends Exception {
+}
